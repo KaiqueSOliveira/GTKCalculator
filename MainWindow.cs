e156ec6ac8661c820580888d4ee1359fc8ae8dfa
Kaique <kaiqueoliveira0101@gmail.com>
2020-10-03 23:41:49 -0300

    working on keys event

--- MainWindow.cs
+++ MainWindow.cs
@@ -3,11 +3,12 @@ using Gtk;
 
 public partial class MainWindow : Gtk.Window
 {
-    int n;
-    int n1;
-    int n2;
-    int res;
+    float n;
+    float n1;
+    float n2;
+    float res;
     bool operatorFlag;
+    bool noneFlag = false;
 
     public MainWindow() : base(Gtk.WindowType.Toplevel)
     {
@@ -17,6 +18,7 @@ public partial class MainWindow : Gtk.Window
         font.Size = 40000;
         numbox.ModifyFont(font);
         numbox.Alignment = 1;
+        numbox.Position = 0;
     }
 
     protected void OnDeleteEvent(object sender, DeleteEventArgs a)                              
@@ -25,27 +27,95 @@ public partial class MainWindow : Gtk.Window
         a.RetVal = true;
     }
 
-    protected void SevenClicked(object sender, EventArgs e)
+
+
+    // On Key Press Events
+
+    protected void OnNumboxKeyPressEvent(object o, KeyPressEventArgs args)
     {
-        n = 7;
-        if (operatorFlag == true)
+        //EventArgs arg = new EventArgs();
+        switch (args.Event.Key)
         {
-            n2 = n;
+            case Gdk.Key.Key_0:
+            case Gdk.Key.KP_0:
+                n = 0;
+                break;
+            case Gdk.Key.Key_1:
+            case Gdk.Key.KP_1:
+                n = 1;
+                break;
+            case Gdk.Key.Key_2:
+            case Gdk.Key.KP_2:
+                n = 2;
+                break;
+            case Gdk.Key.Key_3:
+            case Gdk.Key.KP_3:
+                n = 3;
+                break;
+            case Gdk.Key.Key_4:
+            case Gdk.Key.KP_4:
+                n = 4;
+                break;
+            case Gdk.Key.Key_5:
+            case Gdk.Key.KP_5:
+                n = 5;
+                break;
+            case Gdk.Key.Key_6:
+            case Gdk.Key.KP_6:
+                n = 6;
+                break;
+            case Gdk.Key.Key_7:
+            case Gdk.Key.KP_7:
+                n = 7;
+                break;
+            case Gdk.Key.Key_8:
+            case Gdk.Key.KP_8:
+                n = 8;
+                break;
+            case Gdk.Key.Key_9:
+            case Gdk.Key.KP_9:
+                n = 9;
+                break;
+            case Gdk.Key.BackSpace:                  //<<<<<----------------------------------
+                n = 9;
+                break;
+            default:
+                noneFlag = true;
+                break;
         }
-        else
+
+        if (noneFlag == false)
         {
-            n1 = n;
+            numbox.Text += Convert.ToString(n);
         }
-         
-        numbox.Text += Convert.ToString(n);
+
+        noneFlag = false;
+        numbox.Position = -1;
+
     }
 
-    protected void KeyPress(object sender, KeyPressEventArgs e)
+
+  
+
+        // On Click Events
+
+    protected void OnBtn7Clicked(object sender, EventArgs e)
     {
-        EventArgs args = new EventArgs(); 
-        if (e.Event.Key == Gdk.Key.L7)
-        {
-            SevenClicked(sender,args);
-        }
+        n = 7;
+        numbox.Text += Convert.ToString(n);
+        numbox.Position = -1;
     }
+    protected void OnBtn8Clicked(object sender, EventArgs e)
+    {
+        n = 8;
+        numbox.Text += Convert.ToString(n);
+        numbox.Position = -1;
+    }
+
+    protected void OnSumClicked(object sender, EventArgs e)
+    {
+
+
+    }
+
 }
